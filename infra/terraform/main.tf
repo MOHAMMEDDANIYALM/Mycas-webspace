3bf00cac5ac73fbc3e2462aa29cfc363d5e3dad6
Mohammed Daniyal <certifieddaniyal@gmail.com>
2026-03-26 22:18:27 +0530

    fix(terraform): stabilize web app creation and enforce dynamic vars

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -52,6 +52,7 @@ resource "azurerm_linux_web_app" "backend" {
   resource_group_name = azurerm_resource_group.main.name
   service_plan_id     = azurerm_service_plan.main.id
   https_only          = true
+  depends_on          = [azurerm_linux_web_app.frontend]
 
   site_config {
     always_on = true
